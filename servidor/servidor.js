e92bd02fa2843956706630f4412bded96608b86f
EnzoTofoli <enzortofoli@gmail.com>
2025-04-14 22:04:02 -0300

    Servidor
    Servidor

---
+++ servidor/servidor.js
@@ -0,0 +1,15 @@
+require("colors");
+
+console.log("IMAGINATION".rainbow);
+
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('./public'));
+
+var server = http.createServer(app);
+server.listen(3000);
+
+console.log("servidor rodando...");
